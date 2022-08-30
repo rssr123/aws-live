f83ea856c99155a0c647481dfb07113accb86bd1
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-08-30 14:19:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ck9hfqcyv0yw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "toosweewahemployee"
+customregion = "us-east-1"
