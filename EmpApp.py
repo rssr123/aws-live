6dd5bf6e993e17fc29cee607397ca2d71c6e3089
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-09-27 17:21:03 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -129,7 +129,7 @@ def ViewAllLeave():
 
     for i in range(len(view_records)):
          (emp_id, first_name, last_name, leave_start_date, leave_end_date, leave_reason)=view_records[i]
-         final_emp_id=final_emp_id+emp_id+'\n'
+         final_emp_id=final_emp_id+'\n'.join(emp_id)
          
    # empId=[record[0] for record in view_records]
    # firstName=[record[1] for record in view_records]
@@ -139,7 +139,7 @@ def ViewAllLeave():
   #  leaveReason=[record[5] for record in view_records]
    # for row in view_records:
      #   m = m+row
-    mp_id='\n'.join(final_emp_id)
+   # mp_id='\n'.join(final_emp_id)
     return render_template('ViewAllApplyLeave.html', emp_id=final_emp_id, first_name=first_name,last_name=last_name,leave_start_date=leave_start_date, leave_end_date=leave_end_date, leave_reason=leave_reason)
 
 
