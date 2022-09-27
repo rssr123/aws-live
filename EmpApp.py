56538e959903191c35cd1a46db1c279c5ae93d8c
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-09-28 02:39:01 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -132,7 +132,7 @@ def FetchData():
       db_conn.commit()
       (emp_id, first_name, last_name, pri_skill, location)=emp[0]
       image_url=show_image(custombucket)
-      return render_template('GetEmpOutput.html',id=emp_id,fname=first_name,lname=last_name,interest=pri_skill,location=location,image_url=image_url)
+      return render_template('GetEmpOutput.html',id=emp_id,fname=first_name,lname=last_name,interest=pri_skill,location=location,image_url=https://toosweewahemployee.s3.amazonaws.com/emp-id-2_image_file)
    # except Exception as e:
    #   return render_template('IdNotFound.html')
 
