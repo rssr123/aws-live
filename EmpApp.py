c629b222395e8ef3fd9f8ad38a4590f4e3b43b5f
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-09-28 06:48:20 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -118,7 +118,7 @@ def AddEmp():
             cursor.close()
 
         print("all modification done...")
-            return render_template('AddEmpOutput.html', name=emp_name)
+        return render_template('AddEmpOutput.html', name=emp_name)
     else:
         return render_template('GetEmp.html', name=emp_name)
 
