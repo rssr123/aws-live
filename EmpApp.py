06931be2b0dbe7304dab2f8d2ba2fa5c1df36a34
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-09-27 20:08:08 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -138,15 +138,32 @@ def ViewLeave():
 @app.route("/approveleave", methods=['GET', 'POST'])
 def ApproveLeave():
     eid = request.form['emp_id']
+ #   if(btnApprove){
+  #      lestatus='approve'
+  #  }
+   # else{
+   #      lestatus='reject'
+  #  }
     approve_leave = "Update employee set leave_status='approve' where where emp_id=%s"
     cursor = db_conn.cursor()
-    cursor.execute(approve_leave,(eid))
+    cursor.execute(approve_leave,(lestatus,eid))
+    view_records = cursor.fetchall()
+    db_conn.commit()
+    (emp_id, first_name, last_name, leave_start_date, leave_end_date, leave_reason, leave_status)=view_records[0]
+    return render_template('ApproveLeave.html', emp_id=emp_id, first_name=first_name,last_name=last_name,leave_start_date=leave_start_date, leave_end_date=leave_end_date, leave_reason=leave_reason, leave_status=leave_status)
+
+#below
+@app.route("/approveviewleave", methods=['GET', 'POST'])
+def ApproveViewLeave():
+    view_leave_emp_id = request.form['view_leave_emp_id']
+    view_leave = "Select emp_id, first_name, last_name, leave_start_date, leave_end_date, leave_reason, leave_status from employee where emp_id=%s"
+    cursor = db_conn.cursor()
+    cursor.execute(view_leave,(view_leave_emp_id))
     view_records = cursor.fetchall()
     db_conn.commit()
 
     (emp_id, first_name, last_name, leave_start_date, leave_end_date, leave_reason, leave_status)=view_records[0]
     return render_template('ApproveLeave.html', emp_id=emp_id, first_name=first_name,last_name=last_name,leave_start_date=leave_start_date, leave_end_date=leave_end_date, leave_reason=leave_reason, leave_status=leave_status)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
