d09d9fa39a050d85eb43e917e6df543053b43a26
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-09-27 16:28:40 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -128,8 +128,8 @@ def ViewAllLeave():
     db_conn.commit()
 
     for n in view_records:
-         (emp_id[n], first_name, last_name, leave_start_date, leave_end_date, leave_reason)=view_records[n]
-         final_emp_id=final_emp_id+emp_id[n]
+         (emp_id, first_name, last_name, leave_start_date, leave_end_date, leave_reason)=view_records[n]
+         final_emp_id=final_emp_id+emp_id
    # empId=[record[0] for record in view_records]
    # firstName=[record[1] for record in view_records]
    # lastName=[record[2] for record in view_records]
