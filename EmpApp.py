679090d25d1e5ca5649d55229f45835b343044d2
rssr123 <77654144+rssr123@users.noreply.github.com>
2022-09-28 17:48:08 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -79,8 +79,9 @@ def AddEmp():
         job_title = request.form['job_title']
         gender=request.form['gender']
         date_of_hired=request.form['date_of_hired']
+        salary=2000
 
-        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s)"
+        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s,%s)"
         cursor = db_conn.cursor()
 
         if emp_image_file.filename == "":
@@ -88,7 +89,7 @@ def AddEmp():
 
         try:
 
-            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location,leave_start_date,leave_end_date,leave_reason,leave_status,job_title,gender,date_of_hired))
+            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location,leave_start_date,leave_end_date,leave_reason,leave_status,job_title,gender,date_of_hired,salary))
             db_conn.commit()
             emp_name = "" + first_name + " " + last_name
             # Upload image file in S3 #
